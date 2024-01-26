1f34d36b5a23771f7ef802a848567347a5230d75
Tianhao Zhang <zhangth1990@hotmail.com>
2024-01-25 23:49:12 -0700

    separate file and style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,59 +6,15 @@ import {
     faUser,
     faMagnifyingGlass,
 } from "@fortawesome/free-solid-svg-icons";
-
-const Container = styled.div`
-    position: sticky;
-    top: 0;
-    background-color: ${({ theme }) => theme.bgLighter};
-    height: 56px;
-`;
-const FaBars_Container = styled.div`
-    margin-left: 2%;
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-`;
-const MagnifyingGlass_Container = styled.div`
-    margin-right: 4%;
-`;
-const Wrapper = styled.div`
-    position: relative;
-    display: flex;
-    align-items: center;
-    height: 100%;
-`;
-const Search = styled.div`
-    height: 30px;
-    width: 40%;
-    position: absolute;
-    right: 30%;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 5px;
-    border: 1px solid #ccc;
-    border-radius: 25px;
-`;
-const Input = styled.div`
-    border: none;
-    background-color: transparent;
-`;
-
-const Button = styled.button`
-    padding: 5x 15px;
-    background-color: transparent;
-    border: 1px solid #3ea6ff;
-    color: #3ea6ff;
-    border-radius: 3px;
-    font-weight: 500;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    gap: 5px;
-    position: absolute;
-    right: 2%;
-`;
+import {
+    Container,
+    FaBars_Container,
+    Wrapper,
+    Search,
+    MagnifyingGlass_Container,
+    Input,
+    Button,
+} from "./Header.styled";
 
 function Header() {
     return (
---
+++ src/components/Header/Header.styled.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+export const Container = styled.div`
+    position: sticky;
+    top: 0;
+    background-color: ${({ theme }) => theme.bgLighter};
+    height: 56px;
+`;
+export const FaBars_Container = styled.div`
+    margin-left: 2%;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+`;
+export const MagnifyingGlass_Container = styled.div`
+    margin-right: 4%;
+`;
+export const Wrapper = styled.div`
+    position: relative;
+    display: flex;
+    align-items: center;
+    height: 100%;
+`;
+export const Search = styled.div`
+    height: 30px;
+    width: 40%;
+    position: absolute;
+    right: 30%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5px;
+    border: 1px solid #ccc;
+    border-radius: 25px;
+`;
+export const Input = styled.div`
+    border: none;
+    background-color: transparent;
+`;
+
+export const Button = styled.button`
+    padding: 5x 15px;
+    background-color: transparent;
+    border: 1px solid #3ea6ff;
+    color: #3ea6ff;
+    border-radius: 3px;
+    font-weight: 500;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    position: absolute;
+    right: 2%;
+`;
