6594887947245514e563de9a626ec0e57ed47f6c
Tianhao Zhang <zhangth1990@hotmail.com>
2024-01-27 13:06:23 -0700

    added Recommandation section

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,13 +2,19 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-function Card() {
+function Card({ type }) {
     return (
         <Link to="/video/test" style={{ textDecoration: "none" }}>
-            <Container>
-                <Image src="https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg" />
-                <Details>
-                    <ChannelImage src="https://h5p.org/sites/default/files/h5p/content/1209180/images/file-6113d5f8845dc.jpeg" />
+            <Container type={type}>
+                <Image
+                    type={type}
+                    src="https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg"
+                />
+                <Details type={type}>
+                    <ChannelImage
+                        type={type}
+                        src="https://h5p.org/sites/default/files/h5p/content/1209180/images/file-6113d5f8845dc.jpeg"
+                    />
                     <Texts>
                         <Title>Test Video</Title>
                         <ChannelName>sdfsdf</ChannelName>
@@ -22,27 +28,32 @@ function Card() {
 
 export default Card;
 const Container = styled.div`
-    width: 300px;
-    margin-bottom: 45px;
+    width: ${(props) => props.type !== "sm" && "360px"};
+    margin-bottom: ${(props) => (props.type === "sm" ? "10px" : "45p")};
     cursor: pointer;
     margin-right: 2%;
+    display: ${(props) => props.type === "sm" && "flex"};
+    gap: 10px;
 `;
 
 const Image = styled.img`
     width: 100%;
-    height: 202px;
+    height: ${(props) => (props.type === "sm" ? "120px" : "202px")};
     background-color: #999;
+    flex: 1;
 `;
 const Details = styled.div`
     display: flex;
-    margin-top: 16px;
+    margin-top: ${(props) => (props.type === "sm" ? "0px" : "16px")};
     gap: 12px;
+    flex: 1;
 `;
 const ChannelImage = styled.img`
     width: 36px;
     height: 36px;
     border-radius: 50%;
     background-color: #999;
+    display: ${(props) => props.type === "sm" && "none"};
 `;
 const Texts = styled.div``;
 const Title = styled.h1`
--- src/pages/VideoScreen.js
+++ src/pages/VideoScreen.js
@@ -9,10 +9,89 @@ import {
 import { faShare } from "@fortawesome/free-solid-svg-icons";
 import Comment from "./../components/Comments/Comment";
 import Comments from "../components/Comments/Comments";
+import Card from "../components/Card/Card";
+function VideoScreen() {
+    return (
+        <Container>
+            <VideoContainer>
+                <VideoWrapper>
+                    <iframe
+                        width="100%"
+                        height="100%"
+                        src="https://www.youtube.com/embed/k3Vfj-e1Ma4"
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                    ></iframe>
+                </VideoWrapper>
+                <Title>Test Video</Title>
+                <Details>
+                    <Info>7,948154 views . Jun 22, 2022</Info>
+                    <Buttons>
+                        <Button>
+                            <FontAwesomeIcon icon={faThumbsUp} />
+                            123
+                        </Button>
+                        <Button>
+                            <FontAwesomeIcon icon={faThumbsDown} />
+                            Dislike
+                        </Button>
+                        <Button>
+                            <FontAwesomeIcon icon={faShare} />
+                            Share
+                        </Button>
+                        <Button>
+                            <FontAwesomeIcon icon={faFloppyDisk} />
+                            Save
+                        </Button>
+                    </Buttons>
+                </Details>
+                <Channel>
+                    <ChannelInfo>
+                        <Image src="https://h5p.org/sites/default/files/h5p/content/1209180/images/file-6113d5f8845dc.jpeg" />
+                        <ChannelDetail>
+                            <ChannelName>sdlfk</ChannelName>
+                            <ChannelCounter>200K subscribers</ChannelCounter>
+                        </ChannelDetail>
+                    </ChannelInfo>
+                    <Subscribe>SUBSCRIBE</Subscribe>
+                </Channel>
+                <Description>
+                    sum dolor sit amet, consectetuer adipiscing elit. Aenean
+                    commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+                    penatibus et magnis dis parturient montes, nascetur
+                    ridiculus mus. Donec quam felis, ultricies nec, pellentesque
+                    eu, pretium quis, sem. Nulla consequat massa quis enim.
+                    Donec pede justo,
+                </Description>
+                <Comments />
+            </VideoContainer>
+            <Recommendation>
+                <Card type="sm" />
+                <Card type="sm" />
+                <Card type="sm" />
+                <Card type="sm" />
+                <Card type="sm" />
+                <Card type="sm" />
+                <Card type="sm" />
+            </Recommendation>
+        </Container>
+    );
+}
 
+export default VideoScreen;
 const Container = styled.div`
+    display: flex;
+`;
+
+const VideoContainer = styled.div`
     height: 500px;
     width: 70%;
+`;
+const Recommendation = styled.div`
+    margin-left: 1%;
+    width: 30%;
 `;
 const Content = styled.div`
     flex: 5;
@@ -47,9 +126,7 @@ const Button = styled.div`
     gap: 5px;
     cursor: pointer;
 `;
-const Recommendation = styled.div`
-    flex: 2;
-`;
+
 const Channel = styled.div`
     display: flex;
     justify-content: space-between;
@@ -91,63 +168,3 @@ const Subscribe = styled.button`
     padding: 10px 20px;
     cursor: pointer;
 `;
-
-function VideoScreen() {
-    return (
-        <Container>
-            <VideoWrapper>
-                <iframe
-                    width="100%"
-                    height="100%"
-                    src="https://www.youtube.com/embed/k3Vfj-e1Ma4"
-                    title="YouTube video player"
-                    frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                ></iframe>
-            </VideoWrapper>
-            <Title>Test Video</Title>
-            <Details>
-                <Info>7,948154 views . Jun 22, 2022</Info>
-                <Buttons>
-                    <Button>
-                        <FontAwesomeIcon icon={faThumbsUp} />
-                        123
-                    </Button>
-                    <Button>
-                        <FontAwesomeIcon icon={faThumbsDown} />
-                        Dislike
-                    </Button>
-                    <Button>
-                        <FontAwesomeIcon icon={faShare} />
-                        Share
-                    </Button>
-                    <Button>
-                        <FontAwesomeIcon icon={faFloppyDisk} />
-                        Save
-                    </Button>
-                </Buttons>
-            </Details>
-            <Channel>
-                <ChannelInfo>
-                    <Image src="https://h5p.org/sites/default/files/h5p/content/1209180/images/file-6113d5f8845dc.jpeg" />
-                    <ChannelDetail>
-                        <ChannelName>sdlfk</ChannelName>
-                        <ChannelCounter>200K subscribers</ChannelCounter>
-                    </ChannelDetail>
-                </ChannelInfo>
-                <Subscribe>SUBSCRIBE</Subscribe>
-            </Channel>
-            <Description>
-                sum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-                ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et
-                magnis dis parturient montes, nascetur ridiculus mus. Donec quam
-                felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
-                consequat massa quis enim. Donec pede justo,
-            </Description>
-            <Comments />
-        </Container>
-    );
-}
-
-export default VideoScreen;
