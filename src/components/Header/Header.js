d872a7490701ca79d4e0f357a68d8b7b6dadc088
Tianhao Zhang <zhangth1990@hotmail.com>
2024-03-03 19:15:27 -0700

    add css for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,8 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 import youtubeLogo from "./youtube.png";
 import SearchLogo from "./Search.png";
-
+import UploadLogo from "./upload.png";
+import { IoIosNotificationsOutline } from "react-icons/io";
 function Header() {
     const [name, setName] = useState("");
     return (
@@ -18,7 +19,7 @@ function Header() {
             <IconContainer>
                 <FontAwesomeIcon icon={faBars} size="1x" />
             </IconContainer>
-            <Image src={youtubeLogo} />
+            <YoutubeImage src={youtubeLogo} />
             <SearchContainer>
                 <SearchInput
                     type="text"
@@ -27,15 +28,18 @@ function Header() {
                 />
                 <SearchImage src={SearchLogo} />
             </SearchContainer>
-
-            <Button>
-                <FontAwesomeIcon icon={faUser} />
-                SIGN IN
-            </Button>
+            <UploadImage src={UploadLogo} />
+            <NotificationContainer>
+                <IoIosNotificationsOutline size="30px" />
+            </NotificationContainer>
+            <ProfileImg src="https://plus.unsplash.com/premium_photo-1676637000058-96549206fe71?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
         </Container>
     );
 }
-
+// <Button>
+//     <FontAwesomeIcon icon={faUser} />
+//     SIGN IN
+// </Button>;
 export default Header;
 
 const IconContainer = styled.div`
@@ -43,13 +47,37 @@ const IconContainer = styled.div`
     width: fit-content;
     margin-top: 15px;
 `;
-const Image = styled.img`
+const YoutubeImage = styled.img`
     margin-left: 3%;
     margin-top: 7px;
     width: 100px;
     height: 35px;
 `;
 
+const UploadImage = styled.img`
+    position: absolute;
+    margin-top: 10px;
+    width: 25px;
+    height: 25px;
+    z-index: 10;
+    right: 10.5%;
+`;
+const NotificationContainer = styled.div`
+    position: absolute;
+    margin-top: 8px;
+    right: 6.5%;
+    z-index: 10;
+`;
+
+const ProfileImg = styled.img`
+    position: absolute;
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    margin-top: 8px;
+    right: 2%;
+    z-index: 10;
+`;
 const Container = styled.div`
     top: 0;
     height: 56px;
@@ -63,22 +91,27 @@ const Container = styled.div`
 
 const SearchContainer = styled.div`
     position: relative;
+    margin-left: 10%;
+    margin-top: 5px;
 `;
 
 const SearchInput = styled.input`
     position: absolute;
     border: none;
     background-color: transparent;
-    top: 9px;
-    left: 26px;
+    top: 6px;
+    left: 10px;
     height: 28px;
-    width: 385px;
+    width: 390px;
+    z-index: 2;
 `;
 const SearchImage = styled.img`
     margin-left: 3%;
     margin-top: 2px;
     width: 500px;
-    height: 45px;
+    height: 40px;
+    position: absolute;
+    z-index: 1;
 `;
 const Button = styled.button`
     padding: 5x 15px;
